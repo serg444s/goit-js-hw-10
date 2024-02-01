68f7c964961e80431c22063cf4957812b2c648e8
serg444s <143083342+serg444s@users.noreply.github.com>
2024-02-01 16:44:37 +0200

    task-01

--- src/js/01-timer.js
+++ src/js/01-timer.js
@@ -31,7 +31,6 @@ const options = {
         message: 'Please choose a date in the future',
         position: 'topRight',
       });
-      // window.alert('Please choose a date in the future');
     }
   },
 };
@@ -63,19 +62,14 @@ function addLeadingZero(value) {
 }
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
